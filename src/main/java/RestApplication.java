914add18962deac7d423435bbaabf331a85b6f16
Ivan Núñez Rodriguez <ivannr20@gmail.com>
2025-11-05 10:56:19 +0100

    1

--- src/main/java/RestApplication.java
+++ src/main/java/RestApplication.java
@@ -3,5 +3,3 @@ import jakarta.ws.rs.core.Application;
 
 @ApplicationPath("/api")  //Define que todas las rutas REST comenzarán con /api.
 public class RestApplication extends Application {   //Activa el sistema de servicios web REST sin configuración adicional.
-}
-
